4aeea886a33cb1a350a68c58e401db1496bf654a
Far-Solution <farsolutions3@gmail.com>
2025-10-08 03:30:29 +0500

    fix: added SUPABASE_URL for server build

--- app/api/jitsi-token/route.ts
+++ app/api/jitsi-token/route.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.SUPABASE_URL!;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 const JITSI_DOMAIN = process.env.JITSI_DOMAIN!;
 const JITSI_APP_SECRET = process.env.JITSI_APP_SECRET!;
